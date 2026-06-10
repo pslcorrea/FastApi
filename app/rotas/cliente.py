b92aec8e4c564290ba8efbee8d41aa76d75e10e7
pslcorrea <pslcorrea@gmail.com>
2026-06-10 17:08:54 -0300

    feat: Back end concluido

--- app/rotas/cliente.py
+++ app/rotas/cliente.py
@@ -1,6 +1,13 @@
-from fastapi import APIRouter
+from typing import Annotated
 
-from app.modelos.cliente import Cliente
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+from app.modelos.cliente import Cliente, ClienteCriarAtualizar
+from app.banco_de_dados.cliente_repositorio import ClienteRepositorio
+from app.dependencias import obter_cliente_repositorio
 
 router = APIRouter(
   prefix="/clientes"
@@ -11,12 +18,41 @@ CLIENTE_LIST = [Cliente(id_=1,nome="Paulo Corrêa",email="pslcorrea@gmail.com",t
                    ]
 
 @router.get("/", response_model=list[Cliente])
-async def listar_clientes():
-  return CLIENTE_LIST
+async def listar_clientes(cliente_repositorio: Annotated[ClienteRepositorio, Depends(obter_cliente_repositorio)]):
+  return await cliente_repositorio.listar_clientes()
 
 @router.get("/{cliente_id}", response_model=Cliente | None)
-async def obter_cliente(cliente_id: int):
-  for cliente in CLIENTE_LIST:
-    if cliente.id_ == cliente_id:
-      return cliente
-  return None
+async def obter_cliente(cliente_repositorio: Annotated[ClienteRepositorio, Depends(obter_cliente_repositorio)], cliente_id:int):
+  cliente = await cliente_repositorio.obter_cliente(cliente_id)
+  
+  if not cliente:
+    raise HTTPException(status_code=404, detail="Cliente não encontrado!")
+  
+  return cliente
+
+@router.post("/", response_model=Cliente,status_code=201)
+async def criar_cliente(
+  cliente_repositorio: Annotated[ClienteRepositorio, Depends(obter_cliente_repositorio)],
+  cliente: ClienteCriarAtualizar
+):
+  return await cliente_repositorio.criar_cliente(cliente)
+
+@router.put("/{cliente_id}", response_model=Cliente | None)
+async def atualizar_cliente(
+  cliente_repositorio: Annotated[ClienteRepositorio, Depends(obter_cliente_repositorio)],
+  cliente_id: int, 
+  cliente: ClienteCriarAtualizar
+):
+  cliente_atualizado = await cliente_repositorio.atualizar_cliente(cliente_id, cliente)
+  if not cliente_atualizado:
+    raise HTTPException(status_code=404, detail="Cliente não encontrado!")
+
+  return cliente_atualizado
+
+@router.delete("/{cliente_id}", status_code=204)
+async def deletar_cliente(
+  cliente_repositorio: Annotated[ClienteRepositorio, Depends(obter_cliente_repositorio)], cliente_id: int
+):
+  sucesso = await cliente_repositorio.deletar_cliente(cliente_id)
+  if not sucesso:
+    raise HTTPException(status_code=404, detail="Cliente não encontrado!")
